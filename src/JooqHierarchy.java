0132caf1482dcc1d4d8c41021d72ed8f680ec037
ShobanaNK <snk.shobana@gmail.com>
2025-03-13 18:16:08 +0530

    Joining observation table and build the flat json.

--- src/JooqHierarchy.java
+++ src/JooqHierarchy.java
@@ -1,14 +1,59 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.jooq.*;
+import org.jooq.Record;
 import org.jooq.impl.DSL;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.time.LocalDate;
 import java.time.OffsetDateTime;
+import java.time.OffsetTime;
 import java.util.*;
+import java.util.stream.Collectors;
+
 import static org.jooq.impl.DSL.*;
 
 public class JooqHierarchy {
-    record Content(Long id, String nodeId, List<Content> children) {};
+    record Content(Long id, String nodeId,
+                    Double magnitude,
+                    String unit,
+                    Double numerator,
+                   Double denominator,
+                    String code,
+                    String terminology,
+                    String stringValue,
+                   Boolean booleanValue,
+                   List<Content> children) {};
+
+    record ContentMap(Map<String, Object> resultMap,
+                      List<ContentMap> children) {};
+
+    static Table<?> contents = table("ehr.composition_contents");
+    static Table<?> observations = table("ehr.composition_observation");
+
+
+    static Field<Long> compIdF = DSL.field("composition_id", Long.class);
+    static Field<String> compTypeF = DSL.field("composition_type", String.class);
+    static Field<String> archetypeIdF = DSL.field("archetype_id", String.class);
+    static Field<String> nodeIdF = DSL.field("node_id", String.class);
+    static Field<Integer> memberIndexF = DSL.field("member_index", Integer.class);
+    static Field<Long> parentIdF = DSL.field("parent_id", Long.class);
+    static Field<OffsetDateTime> timeF = DSL.field("time", OffsetDateTime.class);
+
+    static Field<Long> idCF = DSL.field("ehr.composition_contents.id", Long.class);
+
+    static Field<Long> contentIdF = DSL.field("content_id", Long.class);
+    static Field<Double> magnitudeF = DSL.field("magnitude", Double.class);
+    static Field<String> unitF = DSL.field("unit", String.class);
+    static Field<Double> numeratorF = DSL.field("numerator", Double.class);
+    static Field<Double> denominatorF = DSL.field("denominator", Double.class);
+    static Field<String> codedValueF = DSL.field("code", String.class);
+    static Field<String> codedTerminologyF = DSL.field("terminology", String.class);
+    static Field<String> stringValueF = DSL.field("string_value", String.class);
+    static Field<Boolean> booleanValueF = DSL.field("boolean_value", Boolean.class);
+
+    static List<String> nonObservationElements = Arrays.asList(idCF.getName(), parentIdF.getName(), nodeIdF.getName());
+
+
     public static void main(String[] args) {
 
         String url = ConfigLoader.get("db.url");
@@ -17,33 +62,24 @@ public class JooqHierarchy {
 
         try (Connection conn = DriverManager.getConnection(url, user, password)) {
             DSLContext dsl = DSL.using(conn);
-            Table<?> contents = table("ehr.composition_contents");
 
 
-            Field<Long> compIdF = DSL.field("composition_id", Long.class);
-            Field<String> compTypeF = DSL.field("composition_type", String.class);
-            Field<String> archetypeIdF = DSL.field("archetype_id", String.class);
-            Field<String> nodeIdF = DSL.field("node_id", String.class);
-            Field<Integer> memberIndexF = DSL.field("member_index", Integer.class);
-            Field<Long> parentIdF = DSL.field("parent_id", Long.class);
-            Field<OffsetDateTime> timeF = DSL.field("time", OffsetDateTime.class);
-
-            Field<Long> idF = DSL.field("id", Long.class);
-
-            List<Content> hierarchy =
-                    dsl.select(idF, parentIdF, nodeIdF, memberIndexF)
+            List<ContentMap> hierarchy =
+                    dsl.select(idCF, parentIdF, nodeIdF, magnitudeF, unitF, numeratorF, denominatorF, codedValueF, codedTerminologyF, stringValueF, booleanValueF)
                             .from(contents)
-                            .orderBy(idF)
+                            .leftOuterJoin(observations)
+                            .on(idCF.eq(contentIdF)) // Join condition
+                            .orderBy(idCF)
                             .collect(Records.intoHierarchy(
                                     r -> r.value1(),
                                     r -> r.value2(),
-                                    r -> new Content(r.value1(), r.value3(), new ArrayList<>()),
+                                    r -> new ContentMap(r.intoMap(), new ArrayList<>()),
                                     (p, c) -> p.children().add(c)//p.children().put(c.nodeId(), c)
                             ));
 
             System.out.println(hierarchy);
 
-            Map<String, Object> jsonStructure =  convertToJsonStructure("root", hierarchy);
+            Map<String, Object> jsonStructure =  convertToJsonStructure(null, hierarchy);
 
             ObjectMapper objectMapper = new ObjectMapper();
             String json = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonStructure);
@@ -56,25 +92,53 @@ public class JooqHierarchy {
     }
 
 
-    public static Map<String, Object> convertToJsonStructure(String parentString, List<Content> contents) {
+    public static Map<String, Object> convertToJsonStructure(String parentString,List<ContentMap> contents) {
         Map<String, Object> result = new LinkedHashMap<>();
 
-        if(contents.isEmpty()) {
-            result.put(parentString, null);
-        }
-
         Map <String, Integer> cCnt = new HashMap<>();
-        for (Content content : contents) {
+        for (ContentMap content : contents) {
             Integer memIndex = null;
-            if(cCnt.containsKey(content.nodeId())) {
-                memIndex = cCnt.get(content.nodeId());
+            String contentNodeId = (String) content.resultMap().get(nodeIdF.getName());
+            System.out.println(contentNodeId);
+            if(cCnt.containsKey(contentNodeId)) {
+                memIndex = cCnt.get(contentNodeId);
             }
-            String newKey = parentString + "/" + content.nodeId();
+            String newKey = contentNodeId;
             if(memIndex != null) {
                 newKey = newKey + ":" + memIndex;
             }
-            result.putAll(convertToJsonStructure(newKey, content.children()));
-            cCnt.put(content.nodeId(), cCnt.getOrDefault(content.nodeId(), 0) + 1);
+            if (parentString != null) {
+                newKey = parentString + "/" + newKey;
+            }
+
+
+            if (content.children().isEmpty()) {
+                final String keyPrefix = newKey;
+                //result.put(newKey, null);
+                Map<String, Object> observationMap = content.resultMap().entrySet()
+                        .stream()
+                        .filter(entry -> entry.getValue() != null)
+                        .filter(entry -> !nonObservationElements.contains(entry.getKey()) )
+                        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+                // Print the cleaned map
+                System.out.println("Filtered Map: " + observationMap);
+
+                observationMap.forEach( (key, value) -> {
+                    if (key.matches("[a-z]+_value")) {
+                        result.put(keyPrefix, value);
+                    } else {
+                        result.put(keyPrefix + '|' + key, value);
+                    }
+                });
+
+
+
+            } else {
+                result.putAll(convertToJsonStructure(newKey, content.children()));
+            }
+
+            cCnt.put(contentNodeId, cCnt.getOrDefault(contentNodeId, 0) + 1);
         }
         return result;
     }
